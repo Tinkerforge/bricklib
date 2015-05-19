87293a902520362056208119838300dcd45b94c1
Olaf Lüke <olaf@tinkerforge.com>
2015-05-19 18:07:32 +0200

    Enable unflashed Bricklet detection

--- bricklet/bricklet_init.c
+++ bricklet/bricklet_init.c
@@ -274,12 +274,13 @@ uint8_t bricklet_init_plugin(const uint8_t bricklet) {
 
 void bricklet_try_connection(const uint8_t bricklet) {
 	bricklet_select(bricklet);
-/*	const uint32_t magic_number = i2c_eeprom_master_read_magic_number(TWI_BRICKLET);
+	const uint32_t magic_number = i2c_eeprom_master_read_magic_number(TWI_BRICKLET);
 	if(magic_number != BRICKLET_MAGIC_NUMBER) {
 		logbleti("Bricklet %c not connected (wrong magic)\n\r", 'a' + bricklet);
 		bricklet_deselect(bricklet);
 		return;
-	}*/
+	}
+
 	const uint32_t uid = i2c_eeprom_master_read_uid(TWI_BRICKLET);
 	bricklet_deselect(bricklet);
 
